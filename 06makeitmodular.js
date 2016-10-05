e48a303341aba0415efaab697dcc335fb8cc8eda
arvindhmani <amani06@ufl.edu>
2016-10-04 20:51:14 -0400

    working program6

--- 06makeitmodular.js
+++ 06makeitmodular.js
@@ -2,5 +2,12 @@ var mymodule = require('./06mymodule');
 var path = process.argv[2];
 var extn = process.argv[3];
 
-var filesFiltered = mymodule(path, extn);
-console.log(filesFiltered);
+var filesFiltered = mymodule(path, extn, displayFiles);
+
+function displayFiles(err, filesFiltered) {
+	if(err) 
+		console.log(err);
+	else
+		console.log(filesFiltered.join("\r\n"));
+
+}
--- 06mymodule.js
+++ 06mymodule.js
@@ -1,16 +1,15 @@
-var fs = require('fs');
 var path = require('path');
+var fs = require('fs');
 
-module.exports = function(dir, extn, callback) {
-	fs.readdir(dir, function(err, files) {
-		if(err) return callback(err);
-		else {
-			files = files.filter(function(filename) {
-				return path.extname(filename) === '.' + extn;
-			});
-		}
+module.exports = function(pathname, extension, callback) {
+	fs.readdir(pathname, function(err1, files) {
+		if(err1)
+			return callback(err1);
+
+		files = files.filter(function(file) {
+			return path.extname(file) === '.' + extension;
+		});
 
 		callback(null, files);
-	});	
+	});
 }
-
