1042f988587d903ab3cd5c2e37b67950c5aa6fdc
arvindhmani <amani06@ufl.edu>
2016-10-06 02:19:58 -0400

    Working program8

---
+++ 08httpcollect.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const bl = require('bl');
+
+var url = process.argv[2];
+var getReq = http.get(url, function(response) {
+	response.pipe(bl(function(err, data) {
+		if(err) {
+			return console.error(err);
+		}
+
+		console.log(data.length);
+		console.log(data.toString());
+	}));
+});
+
+
